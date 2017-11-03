ef8345a6b42d96313560b562be359a2cd07f7b2f
tdabrowski <dabr@poczta.fm>
2017-11-03 19:17:14 +0100

    refaktoryzacja kodu kalkulatora

--- js/calculator.js
+++ js/calculator.js
@@ -10,28 +10,12 @@ var chairFactory = document.getElementById('chairFactory');
 var chairTypeElements = chairType.children;
 var chairColorElements = chairColor.children;
 var chairFactoryElements = chairFactory.children;
-//List labels
-var typeLable = document.getElementById('typeLable');
-var colorLable = document.getElementById('colorLable');
-var factoryLable = document.getElementById('factoryLable');
 //List button Ids
 var chairTypeButton = document.getElementById('chairTypeButton');
 var chairColorButton = document.getElementById('chairColorButton');
 var chairFactoryButton = document.getElementById('chairFactoryButton');
-//Summary order fields
-var typeSummaryName = document.getElementById('typeSummaryName');
-var colorSummaryName = document.getElementById('colorSummaryName');
-var factorySummaryName = document.getElementById('factorySummaryName');
-var transportSummaryName = document.getElementById('transportSummaryName');
-var typeSummaryPrice = document.getElementById('typeSummaryPrice');
-var colorSummaryPrice = document.getElementById('colorSummaryPrice');
-var factorySummaryPrice = document.getElementById('factorySummaryPrice');
-var transportSummaryPrice = document.getElementById('transportSummaryPrice');
-var summaryCost = document.getElementById('summaryCost');
 //Transport checkbox
 var checkboxTransport = document.getElementById('transport');
-
-
 //Costs
 var totalCost = 0;
 var typeCost = 0;
@@ -39,7 +23,6 @@ var colorCost = 0;
 var factoryCost = 0;
 var transport = 0;
 
-//totalCost = typeCost + colorCost + factoryCost + transport;
 
 //Tables with prices
 var typePrice = [{name:'Chair Clair', price:200},
@@ -53,32 +36,71 @@ var colorPrice = [{name:'Czerwony',price:80},
 var factoryPrice = [{name:'Tkanina',price:80},
                     {name:'Skóra',price:160}];
 
-//client ordered chair (0 - name; 1 - color; 2 - factory)
+//clients chair order (0 - name; 1 - color; 2 - factory)
 var clientOrder = [];
 
 
-//Event click - arrow button for list chairType (drop down menu show on off)
+/**
+* Prints choosen chair elements information in summary field and saves
+* order informaiton in order table.
+* @param {string} lable - id of lable field
+* @param {number} index - id of order table element with place for new information
+* @param {string} summaryName - id of summary element name field
+* @param {string} summaryPrice - id of summary element price field
+* @param {variable} cost - global element cost container
+*/
+function printOrder(lable,index,summaryName,summaryPrice,cost){
+    var elementSummaryName = document.getElementById(summaryName);
+    var elementSummaryPrice = document.getElementById(summaryPrice);
+    var elementLable = document.getElementById(lable);
+    elementLable.innerText = clientOrder[index];
+    elementLable.style.color = '#24ba9f';
+    elementSummaryName.innerText = clientOrder[index];
+    elementSummaryPrice.innerText = cost;
+}
+
+
+/**
+* Prints transport information in summary field
+* @param {string} contentName
+* @param {variable} contentPrice
+*/
+function printTransport(contentName,contentPrice){
+    transportSummaryName.innerText = contentName;
+    transportSummaryPrice.innerText = contentPrice;
+}
+
+
+/**
+* Calculates total cost ordered chair from sum of elements price
+*/
+function caclulateTotalCost(){
+    var summaryCost = document.getElementById('summaryCost');
+    totalCost = typeCost + colorCost + factoryCost + transport;
+    summaryCost.innerText = totalCost;
+}
+
+
+//Event click - arrow button for list chairType (drop down menu show on)
 chairTypeButton.addEventListener('click',function(){
-    chairType.style.display = "block";
+    chairType.style.display = 'block';
 });
 
 
-//Event click - arrow button for list chairColor (drop down menu show on off)
+//Event click - arrow button for list chairColor (drop down menu show on)
 chairColorButton.addEventListener('click',function(){
-    chairColor.style.display = "block";
+    chairColor.style.display = 'block';
 });
 
 
-//Event click - arrow button for list chairFactory (drop down menu show on off)
+//Event click - arrow button for list chairFactory (drop down menu show on)
 chairFactoryButton.addEventListener('click',function(){
-    chairFactory.style.display = "block";
+    chairFactory.style.display = 'block';
 });
-
 
 
 //-------------------------------------------------
 //Event handlers for select lists
-
 //Select type
 for(var i=0; i<chairTypeElements.length; i++){
     chairTypeElements[i].addEventListener('click',function(){
@@ -86,15 +108,11 @@ for(var i=0; i<chairTypeElements.length; i++){
             if(this.dataset.id === typePrice[j].name){
                 typeCost = (typePrice[j].price);
                 clientOrder[0] = typePrice[j].name;
-                typeLable.innerText = clientOrder[0];
-                typeLable.style.color = '#24ba9f';
-                typeSummaryName.innerText = clientOrder[0];
-                typeSummaryPrice.innerText = typeCost;
-                totalCost = typeCost + colorCost + factoryCost + transport;
-                summaryCost.innerText = totalCost;
+                printOrder('typeLable',0,'typeSummaryName','typeSummaryPrice',typeCost);
+                caclulateTotalCost();
             }
         }
-        chairType.style.display = "none";
+        chairType.style.display = 'none';
     });
 }
 
@@ -106,20 +124,13 @@ for(var i=0; i<chairColorElements.length; i++){
             if(this.dataset.id === colorPrice[j].name){
                 colorCost = (colorPrice[j].price);
                 clientOrder[1] = colorPrice[j].name;
-                colorLable.innerText = clientOrder[1];
-                colorLable.style.color = '#24ba9f';
-                colorSummaryName.innerText = clientOrder[1];
-                colorSummaryPrice.innerText = colorCost;
-                totalCost = typeCost + colorCost + factoryCost + transport;
-                summaryCost.innerText = totalCost;
+                printOrder('colorLable',1,'colorSummaryName','colorSummaryPrice',colorCost);
+                caclulateTotalCost();
             }
         }
-        chairColor.style.display = "none";
+        chairColor.style.display = 'none';
     });
 }
-
-
-
 
 //Select factory
 for(var i=0; i<chairFactoryElements.length; i++){
@@ -128,43 +139,30 @@ for(var i=0; i<chairFactoryElements.length; i++){
             if(this.dataset.id === factoryPrice[j].name){
                 factoryCost = (factoryPrice[j].price);
                 clientOrder[2] = factoryPrice[j].name;
-                factoryLable.innerText = clientOrder[2];
-                factoryLable.style.color = '#24ba9f';
-                factorySummaryName.innerText = clientOrder[2];
-                factorySummaryPrice.innerText = factoryCost;
-                totalCost = typeCost + colorCost + factoryCost + transport;
-                summaryCost.innerText = totalCost;
+                printOrder('factoryLable',2,'factorySummaryName','factorySummaryPrice',factoryCost);
+                caclulateTotalCost();
             }
         }
-        chairFactory.style.display = "none";
+        chairFactory.style.display = 'none';
     });
 }
 
 
-
-
-
 //Checking if tranport is ordered
 checkboxTransport.addEventListener('click',function(){
+    var transportSummaryPrice = document.getElementById('transportSummaryPrice');
+    var transportSummaryName = document.getElementById('transportSummaryName');
     if(checkboxTransport.checked === true){
         transport = parseInt(checkboxTransport.dataset.transportPrice);
-        totalCost = typeCost + colorCost + factoryCost + transport;
-        summaryCost.innerText = totalCost;
-        transportSummaryName.innerText = 'Transport';
-        transportSummaryPrice.innerText = transport;
+        caclulateTotalCost();
+        printTransport('Transport',transport);
     }
     else{
         transport = 0;
-        totalCost = typeCost + colorCost + factoryCost + transport;
-        summaryCost.innerText = totalCost;
-        transportSummaryName.innerText = '';
-        transportSummaryPrice.innerText = '';
+        caclulateTotalCost();
+        printTransport('','');
     }
 });
-
-
-
-
 
 
 //end of file
